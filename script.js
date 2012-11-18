0019bcc2e85c6fbf6a31241350889acad69c3d54
timurhai <timurhai@gmail.com>
2012-11-18 21:59:51 +0400

    engine

--- script.js
+++ script.js
@@ -1,54 +1,92 @@
+g_content = 'content/';
+g_path = 'home.html';
+g_navs = [];
+
 function g_Init()
 {
-	document.getElementById('content').innerHTML = 'init';
+	g_Info('g_Init()');
 
-	var nav = document.getElementById('navig').getElementsByTagName('div');
-	for( var i = 0; i < nav.length; i++ )
-	{
-		nav[i].onclick = g_NavClick;
-	}
+	var navs = document.body.getElementsByClassName('navig');
+	for( var i = 0; i < navs.length; i++ ) g_navs.push( navs[i]);
+	navs = document.getElementById('navig').getElementsByTagName('div');
+	for( var i = 0; i < navs.length; i++ ) g_navs.push( navs[i]);
+
+	for( var i = 0; i < g_navs.length; i++ )
+		g_navs[i].onclick = g_NavClick;
+
+	window.onhashchange = g_PathChanged;
+
+	g_PathChanged();
+}
+
+function g_PathChanged()
+{
+	var path = document.location.hash;
+	if( path.indexOf('#') == -1 )
+		path = g_path;
+	else
+		path = path.replace('#','');
+
+	GO( path);
 }
 
 function g_NavClick( i_evt)
 {
 	var nav = i_evt.currentTarget;
-	GO( nav.getAttribute('file'));
+	var path = nav.getAttribute('file');
+	document.location.hash = path;
+	GO( path);
 }
 
 function GO( i_path)
 {
-	document.getElementById('content').innerHTML = i_path;
-	GET(i_path);
+	g_Info( g_path);
+	g_path = i_path;
+
+	for( var i = 0; i < g_navs.length; i++ )
+		if( i_path == g_navs[i].getAttribute('file'))
+			g_navs[i].classList.add('current');
+		else
+			g_navs[i].classList.remove('current');
+
+	document.getElementById('loading').style.display = 'block';
+	GET();
 }
 
-function GET( i_path)
+function g_SetContent( i_data)
+{
+	document.getElementById('content').innerHTML = i_data;
+	document.getElementById('loading').style.display = 'none';
+}
+
+function g_Info( i_msg)
+{
+	document.getElementById('content').innerHTML = i_msg;
+}
+function g_Error( i_msg)
+{
+	g_Info('Error: '+i_msg);
+}
+
+function GET()
 {
 	var xhr = new XMLHttpRequest();
 	xhr.overrideMimeType('application/json');
-//	xhr.setRequestHeader('AFANASY', '1 0 '+obj_str.length);
-	xhr.onerror = function() { document.getElementById('content').innerHTML = xhr.status + ':' + xhr.statusText; }
-	xhr.open('GET', i_path, true);
+	xhr.open('GET', g_content+g_path, true);
 	xhr.send(null);
 
 	xhr.onreadystatechange = function()
 	{
 		if( xhr.readyState == 4 )
 		{
-document.getElementById('content').innerHTML = xhr.status + ':' + xhr.statusText;
+			g_Info( xhr.status + ':' + xhr.statusText);
 			if( xhr.responseText.length )
-				document.getElementById('content').innerHTML = xhr.responseText;
+				g_SetContent( xhr.responseText);
 			else
-				document.getElementById('content').innerHTML = 'Error getting file';
+				g_Error('File '+g_path+' is empty.');
 /*
 			if(( xhr.status == 200 ) && xhr.responseText.length )
 			{
-				log += '<br/><b><i>recv:</i></b> '+ xhr.responseText;
-
-				nw_error_count = 0;
-				nw_connected = true;
-//				g_ProcessMsg( eval('('+xhr.responseText+')'));
-//				g_ProcessMsg( JSON.parse( xhr.responseText));
-
 				var newobj = null;
 				try { newobj = JSON.parse( xhr.responseText);}
 				catch( err)
@@ -61,22 +99,12 @@ document.getElementById('content').innerHTML = xhr.status + ':' + xhr.statusText
 					g_ProcessMsg( JSON.parse( xhr.responseText));
 			}
 			else
-			{
-				nw_error_count++;
-				nw_error_total++;
-//document.getElementById("status").textContent=nw_error_count+': Status number = ' + xhr.status;
-//document.getElementById("statustext").textContent='Status text: ' + xhr.statusText;
-				if(( nw_error_count > nw_error_count_max ) && nw_connected )
-				{
-					nw_connected = false;
-					g_Error('Connection lost.');
-					g_ConnectionLost();
 				}
 			}
 			g_Log( log, 'netlog');
 */
 		}
 		else
-				document.getElementById('content').innerHTML = 'Error getting file';
+			g_Info('Can`t get file '+g_path);
 	}
 }
