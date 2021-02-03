93ad9d80cbf655f0958f203a2e43aae9e69d1b77
timurhai <timurhai@gmail.com>
2021-02-03 11:58:42 +0300

    logo added

--- data_studio_logos.js
+++ data_studio_logos.js
@@ -1,8 +1,9 @@
-// Wed Feb  3 10:40:25 2021
+// Wed Feb  3 11:54:03 2021
 var StudioLogosData = [
 {"logo":"studio_logos/cgf___www.cgfww.com.png"},
 {"logo":"studio_logos/mrp___mrpost.ru.png"},
 {"logo":"studio_logos/platigeimage___www.platige.com.svg"},
+{"logo":"studio_logos/risefx___risefx.com.png"},
 {"logo":"studio_logos/terminalfx___www.terminalfx.com.svg"},
 {"logo":"studio_logos/turborender___turborender.com.png"},
 {"logo":"studio_logos/unitfive___u5fx.ru.png"}
