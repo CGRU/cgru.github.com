5372104a9a89857a2ca1c38c09769ad233af4563
timurhai <timurhai@gmail.com>
2021-11-16 11:45:20 +0300

    logo overmind-studios added

--- data_studio_logos.js
+++ data_studio_logos.js
@@ -1,4 +1,4 @@
-// Tue Apr 20 10:10:47 2021
+// Tue Nov 16 11:44:29 2021
 var StudioLogosData = [
 {"logo":"studio_logos/algous___www.algousstudio.ru.png"},
 {"logo":"studio_logos/artjail___www.artjail.com.png"},
@@ -7,6 +7,7 @@ var StudioLogosData = [
 {"logo":"studio_logos/dobro___dobroccs.com.png"},
 {"logo":"studio_logos/ghostvfx___ghost.dk.svg"},
 {"logo":"studio_logos/mrp___mrpost.ru.png"},
+{"logo":"studio_logos/overmindstudios___www.overmind-studios.de.png"},
 {"logo":"studio_logos/parovoz___parovoz.tv.png"},
 {"logo":"studio_logos/platigeimage___www.platige.com.svg"},
 {"logo":"studio_logos/postkino___postkino.ru.png"},
