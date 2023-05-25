feaaed58df4aa6557ee15827d9b7f0c59c6108ca
timurhai <timurhai@gmail.com>
2023-05-25 15:27:32 +0300

    cgru.info: howtut: animated background removed
    It hangs some browsers.
    Such animation should use buffering (started, but not finished).

--- sites/howtut.cgru.info/bg.js
+++ sites/howtut.cgru.info/bg.js
@@ -1,68 +1,71 @@
-bg_clr_min = 50;
-bg_clr_max = 60;
+var bg_clr_min = 50;
+var bg_clr_max = 60;
 
-var $ = function( id ) { return document.getElementById( id ); };
+var $ = function(id) { return document.getElementById(id); };
 
-bg_elShake = [];
-bg_shake_cycle = 0;
+var bg_elShake = [];
+var bg_shake_cycle = 0;
 
-bg_body_w = 0;
-bg_body_h = 0;
-bg_ctx = null;
-bg_units = [];
-bg_draw_cycle = 0;
+var bg_body_w = 0;
+var bg_body_h = 0;
+var bg_ctx = null;
+var bg_units = [];
+var bg_draw_cycle = 0;
+var bg_canvas = null;
 
 function bg_Init()
 {
-	var canvas = $('canvas');
+	bg_canvas = $('canvas');
 
 	bg_body_w = $('back').clientWidth;
 	bg_body_h = $('back').clientHeight;
 
 	console.log('Ininializing background canvas with resolution: ' + bg_body_h + ' x ' + bg_body_w);
 
-	canvas.width = bg_body_w;
-	canvas.height = bg_body_h;
-	canvas.style.width = bg_body_w + 'px';
-	canvas.style.height = bg_body_h + 'px';
+	bg_canvas.width = bg_body_w;
+	bg_canvas.height = bg_body_h;
+	bg_canvas.style.width = bg_body_w + 'px';
+	bg_canvas.style.height = bg_body_h + 'px';
 
-	bg_ctx = canvas.getContext('2d');
+	bg_ctx = bg_canvas.getContext('2d');
 	bg_ctx.lineWidth = 1.0;
-	for( var i = 0; i < bg_body_h; i++)
+	for (let i = 0; i < bg_body_h; i++)
 	{
-		var clr = Math.floor( bg_clr_min + .5*(bg_clr_max - bg_clr_min) * Math.random());
+		let clr = Math.floor(bg_clr_min + .5*(bg_clr_max - bg_clr_min) * Math.random());
 		bg_ctx.strokeStyle = 'rgb('+clr+','+clr+','+clr+')';
 		bg_ctx.beginPath();
-		bg_ctx.moveTo( 0, i+.5);
-		bg_ctx.lineTo( bg_body_w, i+.5);
+		bg_ctx.moveTo(0, i+.5);
+		bg_ctx.lineTo(bg_body_w, i+.5);
 		bg_ctx.stroke();
 	}
 
 	bg_elShake = document.getElementsByClassName('shake');
-	for( var i = 0; i < bg_elShake.length; i++)
+	for (let i = 0; i < bg_elShake.length; i++)
 	{
 		bg_elShake[i].onmouseover = bg_OnMouseOver;
 		bg_elShake[i].onmousemove = bg_OnMouseMove;
 		bg_elShake[i].onmouseout = bg_OnMouseOut;
 	}
-
 	bg_Shake();
-	for( var i = 0; i < 100; i++ ) bg_Draw( true);
-	bg_Draw();
 
 	document.body.onclick = bg_BodyOnClick;
 	document.body.onmousemove = bg_BodyOnMouseMove;
+
+	for (let i = 0; i < 100; i++)
+		bg_Draw(true);
+
+	bg_Draw();
 }
 
 function bg_OnMouseMove(i_e)
 {
 	i_e.currentTarget.m_shake = 1;
 
-	var x = i_e.clientX;
-	var y = i_e.clientY;
-	var dx = 8;
-	var dy = 4;
-	bg_units.push( new Unit({"x_min":x-dx,"x_max":x+dx,"y_min":y-dy,"y_max":y+dy,"clr_mult":4,"type":1}));
+	let x = i_e.clientX;
+	let y = i_e.clientY;
+	let dx = 8;
+	let dy = 4;
+	bg_units.push(new Unit({"x_min":x-dx,"x_max":x+dx,"y_min":y-dy,"y_max":y+dy,"clr_mult":4,"type":1}));
 }
 function bg_OnMouseOver(i_e)
 {
@@ -75,109 +78,122 @@ function bg_OnMouseOut(i_e)
 
 function bg_Shake()
 {
-	var amp = 4.0 / ( 1 + .3*bg_shake_cycle );
-	var opa = 1.0 - 1.0 / ( 2 + .3*bg_shake_cycle );
+	let amp = 4.0 / (1 + .3*bg_shake_cycle);
+	let opa = 1.0 - 1.0 / (2 + .3*bg_shake_cycle);
 	bg_shake_cycle++;
 
-	for( var i = 0; i < bg_elShake.length; i++)
+	for (let i = 0; i < bg_elShake.length; i++)
 	{
-		var el = bg_elShake[i];
+		let el = bg_elShake[i];
 
-		if(( el.m_shake == null ) || ( el.m_shake == 0 ))
+		if ((el.m_shake == null) || (el.m_shake == 0))
 			continue;
 
-		var amp = 4.0 / ( 1 + .1 * el.m_shake );
+		let amp = 4.0 / (1 + .1 * el.m_shake);
 		el.m_shake += 1.0;
 
-		if( isNaN( amp ) || ( amp < .1 ))
+		if (isNaN(amp) || (amp < .1))
 		{
 			el.m_shake = 0;
 			continue;
 		}
 
-		var value = (Math.floor( amp * Math.random())) + 'px';
-		el.style.top = value;
+		el.style.top = (Math.floor(amp * Math.random())) + 'px';
 
-		var value = (Math.floor( .5 * amp * Math.random())) + 'px';
-		el.style.left = value;
+		el.style.left = (Math.floor(.5 * amp * Math.random())) + 'px';
 	}
 
-//	if( bg_shake_cycle < 100 )
-		setTimeout( bg_Shake, 50);
+//	if (bg_shake_cycle < 100)
+		setTimeout(bg_Shake, 50);
 }
 
-function bg_BodyOnMouseMove( e )
+function bg_BodyOnMouseMove(e)
 {
-	var x = e.clientX;
-	var y = e.clientY;
-	var dx = 4;
-	var dy = 4;
-	bg_units.push( new Unit({"x_min":x-dx,"x_max":x+dx,"y_min":y-dy,"y_max":y+dy}));
+	let x = e.clientX;
+	let y = e.clientY;
+	let dx = 4;
+	let dy = 4;
+	bg_units.push(new Unit({"x_min":x-dx,"x_max":x+dx,"y_min":y-dy,"y_max":y+dy}));
 }
 
-function bg_BodyOnClick( e )
+function bg_BodyOnClick(e)
 {
-	var x = e.clientX;
-	var y = e.clientY;
-	var dx = 4;
-	var dy = 4;
-	for( var i = 0; i < 4; i++)
-		bg_units.push( new Unit({"x_min":x-dx,"x_max":x+dx,"y_min":y-dy,"y_max":y+dy,"clr_mult":4,"type":1}));
+	let x = e.clientX;
+	let y = e.clientY;
+	let dx = 4;
+	let dy = 4;
+	for (let i = 0; i < 4; i++)
+		bg_units.push(new Unit({"x_min":x-dx,"x_max":x+dx,"y_min":y-dy,"y_max":y+dy,"clr_mult":4,"type":1}));
 }
 
-function bg_Draw( i_no_timeout )
+function bg_Draw(i_no_timeout)
 {
+	let time_start = new Date();
+
 	bg_draw_cycle++;
 
-	while( bg_units.length < 100 )
+	while(bg_units.length < 100)
 	{
-		bg_units.push( new Unit({"x_min":0,"x_max":bg_body_w,"y_min":0,"y_max":bg_body_h}));
+		bg_units.push(new Unit({"x_min":0,"x_max":bg_body_w,"y_min":0,"y_max":bg_body_h}));
 	}
 
-	if(( bg_draw_cycle % 10 ) == 0 )
+	if ((bg_draw_cycle % 10) == 0)
 		bg_units.shift();
 
-	for( var i = 0; i < bg_units.length; i++)
+	//let context = bg_canvas.getContext('2d');
+	//let image = context.createImageData(bg_body_w, bg_body_h);
+	//let image = context.getImageData(0, 0, bg_body_w, bg_body_h);
+	let image = bg_ctx.getImageData(0, 0, bg_body_w, bg_body_h);
+
+	for (let i = 0; i < bg_units.length; i++)
 	{
-		bg_units[i].refresh();
-		bg_units[i].draw();
+		bg_units[i].refresh(i==0);
+		bg_units[i].draw(image.data, i==0);
 	}
 
-	if( i_no_timeout !== true )
-		setTimeout( bg_Draw, 100);
+//	for (let i = 0; i < 40; i++) image.data[1000*bg_draw_cycle + i] = 255;
+
+
+	//context.putImageData(image, 0, 0);
+	bg_ctx.putImageData(image, 0, 0);
+
+	if (i_no_timeout !== true)
+		setTimeout(bg_Draw, 100);
+
+//console.log(bg_draw_cycle + '[' + bg_units.length + ']: ' + ((new Date()) - time_start) + 'ms');
 }
 
-function Unit( i_args)
+function Unit(i_args)
 {
-	if( i_args.clr_mult == null )
+	if (i_args.clr_mult == null)
 		i_args.clr_mult = 1;
 
 	this.type = i_args.type;
 	this.cycle = 0;
 
-	this.x = i_args.x_min + ( i_args.x_max - i_args.x_min ) * Math.random();
-	this.y = i_args.y_min + ( i_args.y_max - i_args.y_min ) * Math.random();
+	this.x = i_args.x_min + (i_args.x_max - i_args.x_min) * Math.random();
+	this.y = i_args.y_min + (i_args.y_max - i_args.y_min) * Math.random();
 
-	if( Math.random() > .5 )
+	if (Math.random() > .5)
 	{
-		this.vx = ( Math.random() > .5 ) ? 1 : -1;;
+		this.vx = (Math.random() > .5) ? 1 : -1;;
 		this.vy = 0;
 	}
 	else
 	{
 		this.vx = 0;
-		this.vy = ( Math.random() > .1 ) ? 1 : -1;;
+		this.vy = (Math.random() > .1) ? 1 : -1;;
 	}
 
 	this.img = bg_ctx.createImageData(2,2);
 
-	this.setColor( i_args.clr_mult);
+	this.setColor(i_args.clr_mult);
 }
-Unit.prototype.setColor = function( i_mult)
+Unit.prototype.setColor = function(i_mult)
 {
-	if( i_mult == null ) i_mult = 1;
-	var clr = bg_clr_min + i_mult * (bg_clr_max - bg_clr_min) * Math.random();
-	for( var i = 0; i < 4; i++)
+	if (i_mult == null) i_mult = 1;
+	let clr = bg_clr_min + i_mult * (bg_clr_max - bg_clr_min) * Math.random();
+	for (let i = 0; i < 4; i++)
 	{
 		this.img.data[i*4+0] = clr;
 		this.img.data[i*4+1] = clr;
@@ -187,44 +203,63 @@ Unit.prototype.setColor = function( i_mult)
 }
 Unit.prototype.refresh = function()
 {
-	var to_y = .01;
-	var to_x = .1;
-	var clr = .5;
+	let to_y = .01;
+	let to_x = .1;
+	let clr = .5;
 
-	if( this.type )
+	if (this.type)
 	{
 		to_x = .001 * this.cycle;
-		if( to_x > .1 ) to_x = .1;
+		if (to_x > .1) to_x = .1;
 		clr = 1;
 	}
 
-	if(( Math.random() < to_y ) && ( this.vy == 0 ))
+	if ((Math.random() < to_y) && (this.vy == 0))
 	{
 		this.vx = 0;
-		this.vy = ( Math.random() > .1 ) ? 1 : -1;
+		this.vy = (Math.random() > .1) ? 1 : -1;
 
-		if( Math.random() < clr )
+		if (Math.random() < clr)
 			this.setColor();
 	}
-	else if(( Math.random() < to_x ) && ( this.vx == 0 ))
+	else if ((Math.random() < to_x) && (this.vx == 0))
 	{
-		this.vx = ( Math.random() > .5 ) ? 1 : -1;
+		this.vx = (Math.random() > .5) ? 1 : -1;
 		this.vy = 0;
 	}
 
 	this.x += this.vx;
 	this.y += this.vy;
 
-	if( this.x > bg_body_w ) this.x = 0;
-	if( this.y > bg_body_h ) this.y = 0;
+	if (this.x > bg_body_w) this.x = 0;
+	if (this.y > bg_body_h) this.y = 0;
 
-	if( this.x < 0 ) this.x = bg_body_w;
-	if( this.y < 0 ) this.y = bg_body_h;
+	if (this.x < 0) this.x = bg_body_w;
+	if (this.y < 0) this.y = bg_body_h;
 
 	this.cycle++;
 }
-Unit.prototype.draw = function()
+Unit.prototype.draw = function(i_data, iv)
 {
-	bg_ctx.putImageData( this.img, this.x, this.y );
+//	bg_ctx.putImageData(this.img, this.x, this.y);
+return;
+if (iv)
+	console.log(this.x + ',' + this.y);
+
+	let index = 4 * (bg_body_w * this.y + this.x);
+	for (let i = 0; i < 4; i++)
+		i_data[index + i] = 255;
+
+	index = 4 * (bg_body_w * this.y + this.x+1);
+	for (let i = 0; i < 4; i++)
+		i_data[index + i] = 255;
+
+	index = 4 * (bg_body_w * (this.y+1) + this.x+1);
+	for (let i = 0; i < 4; i++)
+		i_data[index + i] = 255;
+
+	index = 4 * (bg_body_w * (this.y+1) + this.x);
+	for (let i = 0; i < 4; i++)
+		i_data[index + i] = 255;
 }
 
